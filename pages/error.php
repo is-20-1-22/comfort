8bbc8fce765581cafc2c6f0871b34fdbd023a171
is-20-1-22 <120209358+is-20-1-22@users.noreply.github.com>
2023-04-25 04:22:56 +0500

    Add files via upload

---
+++ pages/error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_error.css">
+    <title>Ошибочка</title>
+</head>
+<body>
+<!------------------------------------------------------------------- main html ------------------------------------------------------------------------->
+<main>
+    <div class="container">
+        <ul>
+            <li><h1>4</h1></li>
+            <li><img src="img/icons/error.png" alt="" width="345px"></li>
+            <li><h1>4</h1></li>
+        </ul>
+        <div class="text">Мы не смогли найти то, что вы искали.</br>Но, возможно, оно где-то рядом..</div>
+        <button><a href="index.html">←</a></button>
+    </div>
+</main>
+</body>
+</html>
